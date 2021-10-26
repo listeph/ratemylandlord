7bf610e6dfdccdf32da3266aac19a7bbcf946e68
listeph <20steph18@gmail.com>
2021-10-26 16:39:41 -0400

    added a component file for each page

---
+++ rate_my_landlord/frontend/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import SearchByNamePage from './SearchByNamePage';
+import FilterByRatingsPage from './FilterByRatingsPage';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/'><p>This is the home page</p></Route>
+                    <Route path='/search' component={SearchByNamePage} />
+                    <Route path='/filter' component={FilterByRatingsPage} />
+                </Switch>
+            </Router>
+        );
+    }
+}
