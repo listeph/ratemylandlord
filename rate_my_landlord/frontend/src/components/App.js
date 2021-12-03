7a458d0ab07719c914d4413c626d6fcb8872e622
Kevin Lee <leek16@github.com>
2021-12-03 01:03:48 -0500

    Header added and working

--- rate_my_landlord/frontend/src/components/App.js
+++ rate_my_landlord/frontend/src/components/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from './HomePage';
+import Header from './Header';
+import { Router } from "@material-ui/icons";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
 
-    render() {
+    render()
+    {
         return (
-            <div className="center">
-                <HomePage />
+            <div>
+                <div className = "center">
+                    <HomePage />
+                </div>
+
+                <div className = "Header">
+                    <Header />
+                </div>
+                
             </div>
         );
     }
+
 }
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
+
+
+
+
+
+
+
+
