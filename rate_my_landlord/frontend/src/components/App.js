12e95b9089327fe0f8c816d30fb23bb9170bf083
Steph Li <20steph18@gmail.com>
2021-12-03 02:16:25 -0500

    Revert "Kevinbranch"

--- rate_my_landlord/frontend/src/components/App.js
+++ rate_my_landlord/frontend/src/components/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from './HomePage';
-import Header from './Header';
-import { Router } from "@material-ui/icons";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
 
-    render()
-    {
+    render() {
         return (
-            <div>
-                <div className = "center">
-                    <HomePage />
-                </div>
-
-                <div className = "Header">
-                    <Header />
-                </div>
-                
+            <div className="center">
+                <HomePage />
             </div>
         );
     }
-
 }
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
-
-
-
-
-
-
-
-
